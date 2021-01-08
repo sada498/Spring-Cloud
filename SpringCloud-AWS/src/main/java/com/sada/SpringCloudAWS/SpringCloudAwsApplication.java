43e40619afc65e1dfe83dd30b0bc20af886cde6e
sada498 <sada.y@outlook.com>
2021-01-08 23:58:54 +1100

    v

--- SpringCloud-AWS/src/main/java/com/sada/SpringCloudAWS/SpringCloudAwsApplication.java
+++ SpringCloud-AWS/src/main/java/com/sada/SpringCloudAWS/SpringCloudAwsApplication.java
@@ -4,17 +4,18 @@ import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = ContextInstanceDataAutoConfiguration.class)
 public class SpringCloudAwsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudAwsApplication.class, args);
-		System.out.println("hello");
+		//System.out.println("hello");
 	}
 	@Bean
-	ApplicationRunner applicationRunner(AmazonS3 amazonS3) {
+	ApplicationRunner applicationRunner(AmazonS3 ) {
 		return args -> {
 			amazonS3.listBuckets().forEach(bucket -> System.out.println(bucket.getName()));
 		};
